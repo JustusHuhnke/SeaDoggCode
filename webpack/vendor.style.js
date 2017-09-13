122ac24d8b4bd7260906de611dae29daca147ae7
Ihor Haiduk <cookkycok@gmail.com>
2017-09-13 22:26:25 +0300

    fix style

---
+++ webpack/vendor.style.js
@@ -0,0 +1,8 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var resolve = require('path').resolve;
+var vendor = [resolve(__dirname, "..", "node_modules", "react-select/dist/react-select.css")];
+exports.default = vendor;
