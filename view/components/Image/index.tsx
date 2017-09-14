39bef75edda3002312717aec789dca2359f47882
Ihor Haiduk <cookkycok@gmail.com>
2017-09-15 00:02:04 +0300

    59b92cd749b203f298ff4a3b Image Component

--- view/components/Image/index.tsx
+++ view/components/Image/index.tsx
@@ -1,15 +1,35 @@
+import {Source} from "_components/Image/source";
 import {component} from "_style";
-// import request from "_utils/xhr";
-// import * as cx from "classnames";
+import * as cx from "classnames";
+import {List} from "immutable";
 import * as React from "react";
 import {IImageComponent} from "./interface";
+
+const defaultSizes = [
+    {
+        name: "2k",
+        media: "(min-width: 1921px)",
+    }, {
+        name: "full",
+        media: "(min-width: 1367px) and (max-width: 1920px)",
+    }, {
+        name: "hd",
+        media: "(min-width: 769px) and (max-width: 1366px)",
+    }, {
+        name: "wide",
+        media: "(min-width: 481px) and (max-width: 768px)",
+    }, {
+        name: "half",
+        media: "(max-width: 480px)",
+    },
+];
 
 export class ImageComponent extends React.PureComponent<IImageComponent, undefined> {
 
     public static defaultProps: IImageComponent = {
         className: component.image,
-        viewBox: "0 0 24 24",
         src: null,
+        alt: "",
     };
 
     public componentDidMount() {
@@ -20,16 +40,24 @@ export class ImageComponent extends React.PureComponent<IImageComponent, undefin
     }
 
     public render() {
-        return (
-            <picture>
-                <source srcSet="http://via.placeholder.com/350x150?text=2K" media="(min-width: 1921px)"/>
-                <source srcSet="http://via.placeholder.com/350x150?text=1920px" media="(min-width: 1367px) and (max-width: 1920px)"/>
-                <source srcSet="http://via.placeholder.com/350x150?text=1366px" media="(min-width: 769px) and (max-width: 1366px)"/>
-                <source srcSet="http://via.placeholder.com/350x150?text=768px" media="(min-width: 481px) and (max-width: 768px)"/>
-                <source srcSet="http://via.placeholder.com/350x150?text=480px" media="(max-width: 480px)"/>
-                <img srcSet="http://via.placeholder.com/350x150?text=1920px" alt="My default image"/>
-            </picture>
-        );
+        const {className, custom, src, alt, ...otherProps} = this.props;
+        const classes = cx(className);
+
+        if (List.isList(custom)) {
+            return (
+                <picture className={classes} {...otherProps}>
+                    {custom.map((props, key) => <Source key={key} {...props} />).toArray()}
+                    <img srcSet={src} alt={alt}/>
+                </picture>
+            );
+        } else {
+            return (
+                <picture className={classes} {...otherProps}>
+                    {defaultSizes.map(({media, name}, key) => <Source key={key} src={`${src}${name}`} media={media}/>)}
+                    <img srcSet={src} alt={alt}/>
+                </picture>
+            );
+        }
     }
 }
 
