5a2512cafb16bda0348e07695661291a7b82b64b
Ihor Haiduk <cookkycok@gmail.com>
2017-09-16 14:26:17 +0300

    59bc0a46be97580691cb7572 Button component

--- view/components/ButtonComponent/index.tsx
+++ view/components/ButtonComponent/index.tsx
@@ -3,29 +3,28 @@ import {PureComponent} from "_components/PureComponent";
 import {component} from "_style";
 import * as cx from "classnames";
 import * as React from "react";
-import {Link} from "react-router-dom";
-import {ILinkComponent} from "./interface";
+import {IButtonComponent} from "./interface";
 
-export class LinkComponent extends React.PureComponent<ILinkComponent, undefined> {
+export class ButtonComponent extends React.PureComponent<IButtonComponent, undefined> {
 
-    public static defaultProps: ILinkComponent = {
-        href: "/",
+    public static defaultProps: IButtonComponent = {
         className: component.link,
+        type: "button",
     };
 
     public render() {
 
-        const {href, rel, children, className, icon, iconClass, title, titleClass, ...otherProps} = this.props;
+        const {type, children, className, icon, iconClass, title, titleClass, ...otherProps} = this.props;
         const classes = cx(className);
 
         return (
-            <Link to={href} className={classes} rel={rel} {...otherProps}>
+            <button className={classes} type={type} {...otherProps}>
                 {!!children && children}
                 {!children && !!icon && <IconComponent name={icon} className={iconClass} />}
                 {!children && !!title && <PureComponent tag="span" className={titleClass}>{title}</PureComponent>}
-            </Link>
+            </button>
         );
     }
 }
 
-export default LinkComponent;
+export default ButtonComponent;
