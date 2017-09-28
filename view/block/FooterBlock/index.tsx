f1bf4c77ef3d8171a708e818767afeded94b2095
Alexey <alexeyvakarchuk@yandex.ru>
2017-09-28 15:41:05 +0300

    Add media queries and styling for footer

--- view/block/FooterBlock/index.tsx
+++ view/block/FooterBlock/index.tsx
@@ -1,10 +1,15 @@
+import {IconComponent} from "_components/IconComponent";
+import {LinkComponent} from "_components/LinkComponent";
+import {LogoComponent} from "_components/LogoComponent";
 import {NavigationComponent} from "_components/NavigationComponent";
 import {PureComponent} from "_components/PureComponent";
-import {block, section} from "_style";
+import {block, component, section} from "_style";
 import Rbem from "_utils/rbem";
 import * as React from "react";
+// import {component} from "_stylesLoad/index";
 
 const footer = new Rbem(block, "footer");
+const icon = new Rbem(component, "icon");
 
 export class FooterBlock extends React.Component<{}, {}> {
 
@@ -16,11 +21,29 @@ export class FooterBlock extends React.Component<{}, {}> {
                         <div className={footer.get("nav")}>
                             <NavigationComponent />
                             <div className={footer.get("social")}>
-                                Follow us on:
+                                <p className={footer.get("text")}>Follow us:</p>
+                                <LinkComponent href="http://google.com" target="_blank">
+                                    <IconComponent name="facebook" viewBox="0 0 12 24" />
+                                </LinkComponent>
+                                <LinkComponent href="http://google.com" target="_blank">
+                                    <IconComponent name="twitter" viewBox="0 0 25 20" />
+                                </LinkComponent>
+                                <LinkComponent href="http://google.com" target="_blank">
+                                    <IconComponent name="google_plus" viewBox="0 0 24 24" />
+                                </LinkComponent>
+                                <LinkComponent href="http://google.com" target="_blank">
+                                    <IconComponent name="vine" viewBox="0 0 25 22" />
+                                </LinkComponent>
+                                <LinkComponent href="http://google.com" target="_blank">
+                                    <IconComponent name="youtube" viewBox="0 0 25 22" className={icon.get(null, "youtube")} />
+                                </LinkComponent>
                             </div>
                         </div>
 
-                        {/*<p className={block.footer__text}>SeaDogg, 2017. All rights reserved</p>*/}
+                        <div className={footer.get("info")}>
+                            <div className={footer.get("text")}><LogoComponent />We  revolutionize water expiriences</div>
+                            <div className={footer.get("copyright")}>© All rights reserved. A Seadogg Inc. Company 2017</div>
+                        </div>
                     </div>
                 </div>
             </PureComponent>
