d98f46c5d727f55788e970e6e863857c83df9171
Ihor Haiduk <cookkycok@gmail.com>
2017-09-30 20:33:36 +0300

    59ce95bc35a97bfbdf16dd1a SVG-map with tooltip

--- view/block/WorldMapBlock/interface.ts
+++ view/block/WorldMapBlock/interface.ts
@@ -1,5 +1,22 @@
-import {ReactNode} from "react";
+import {List} from "immutable";
 
-export interface IContact {
-    children?: ReactNode;
+export interface IWorldPoint {
+    x: number;
+    y: number;
+}
+export interface IWorldLocations {
+    code: string;
+    title: string;
+    description: string;
+    avatar: string;
+    user_name: string;
+    user_description: string;
+    location: string;
+    tel: string;
+    mail: string;
+    point: IWorldPoint;
+}
+
+export interface IWorldState {
+    locations: List<IWorldLocations>;
 }
